8c1ea0bb4b185032fc42b29252d181d8c1b5ce44
sonnt2990 <sonnt@zitga.com.vn>
2021-03-03 23:22:20 +0700

    3/3/2021 11:22PM

--- demochatReal/Client/src/main/java/com/zitgacore/chat/config/game/GameConfig.java
+++ demochatReal/Client/src/main/java/com/zitgacore/chat/config/game/GameConfig.java
@@ -4,32 +4,18 @@ import com.zitga.bean.annotation.BeanConfiguration;
 
 @BeanConfiguration("config/game.properties")
 public class GameConfig {
-	private String host="localhost";
-    private int port=8101;
-    private String secret="WzdbC3PQqzGgZYCA";
-
-    
-    
-//    public String getHost() {
-//		return host;
-//	}
-//
-//	public int getPort() {
-//		return port;
-//	}
-//
-//	public String getSecret() {
-//		return secret;
-//	}
+	private String host;
+	private int port;
+	private String secret;
 
 	private ConnectorConfig master;
 
-    public ConnectorConfig getMaster() {
-        return master;
-    }
+	public ConnectorConfig getMaster() {
+		return master;
+	}
 
-    @Override
-    public String toString() {
-        return "GameConfig{ master=" + master +'}';
-    }
+	@Override
+	public String toString() {
+		return "GameConfig{ master=" + master + '}';
+	}
 }
